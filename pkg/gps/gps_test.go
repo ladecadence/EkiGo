e4fd138c3c43c76fe7851d08ee8117814ccce5fc
David Pello <dvpg@tuta.io>
2025-04-26 17:30:44 +0200

    GPS Test

---
+++ pkg/gps/gps_test.go
@@ -0,0 +1,27 @@
+package gps
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGPS(t *testing.T) {
+	gps, err := New("/dev/ttyAMA0", 9600)
+	if err != nil {
+		t.Errorf("Error starting GPS: %v", err)
+	}
+
+	if gps != nil {
+		defer gps.Close()
+		fmt.Println("GPS Started")
+		err := gps.Update()
+		if err != nil {
+			t.Errorf("Error updating GPS: %v", err)
+		}
+		h, m, s, err := gps.Time()
+		if err != nil {
+			t.Errorf("Error reading GPS time: %v", err)
+		}
+		fmt.Printf("GPS time: %v %v %v\n", h, m, s)
+	}
+}
