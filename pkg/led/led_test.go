55af73fec5e55fd8e6b3554bf96478ea7cc0dd11
David Pello <dvpg@tuta.io>
2025-04-26 13:40:00 +0200

    LED test

---
+++ pkg/led/led_test.go
@@ -0,0 +1,28 @@
+package led
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestLed(t *testing.T) {
+	led, err := New(17)
+	if err != nil {
+		t.Errorf("Problem creating LED: %v", err)
+	}
+
+	// test LED
+	fmt.Println("Normal blink")
+	err = led.Blink()
+	if err != nil {
+		t.Errorf("Problem blinking LED: %v", err)
+	}
+
+	time.Sleep(time.Second * 2)
+	// error
+	err = led.BlinkError()
+	if err != nil {
+		t.Errorf("Problem error blinking LED: %v", err)
+	}
+}
