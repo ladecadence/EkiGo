acc8afbb1340b2d4089574a1f97b5702a3fc8a31
David Pello <dvpg@tuta.io>
2025-04-26 17:18:34 +0200

    Fix conversion using rxBuf

--- pkg/mcp3002/mcp3002.go
+++ pkg/mcp3002/mcp3002.go
@@ -102,9 +102,9 @@ func (m *mcp3002) Read(channel uint8) (uint32, error) {
 
 	// convert value
 	var result uint32
-	result = (uint32(txBuf[0]) & 0x01) << 9
-	result |= (uint32(txBuf[1]) & 0xff) << 1
-	result |= (uint32(txBuf[2]) & 0x80) >> 7
+	result = (uint32(rxBuf[0]) & 0x01) << 9
+	result |= (uint32(rxBuf[1]) & 0xff) << 1
+	result |= (uint32(rxBuf[2]) & 0x80) >> 7
 	fmt.Println(txBuf)
 	fmt.Println(rxBuf)
 
