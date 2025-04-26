67a13089d0027ca82c0cf4698dc52def70e68d16
David Pello <dvpg@tuta.io>
2025-04-26 16:35:15 +0200

    Convert to use periph.io library

--- pkg/mcp3002/mcp3002.go
+++ pkg/mcp3002/mcp3002.go
@@ -3,7 +3,10 @@ package mcp3002
 import (
 	"errors"
 
-	"github.com/stianeikeland/go-rpio/v4"
+	"periph.io/x/conn/v3/driver/driverreg"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
 )
 
 type MCP3002 interface {
@@ -17,23 +20,48 @@ type mcp3002 struct {
 
 func New(cs uint8, ch int) (MCP3002, error) {
 	adc := mcp3002{csel: cs, channel: ch}
+
 	// test that we can access the device
-	err := rpio.Open()
-	if err != nil {
+	if _, err := driverreg.Init(); err != nil {
 		return nil, err
 	}
-	var channel rpio.SpiDev
+
+	var spiChan = ""
 	if adc.channel == 0 {
-		channel = rpio.Spi0
+		spiChan = "0"
 	} else {
-		channel = rpio.Spi1
+		spiChan = "1"
 	}
-	err = rpio.SpiBegin(channel)
+
+	// open port
+	p, err := spireg.Open(spiChan)
 	if err != nil {
 		return nil, err
 	}
-	rpio.SpiEnd(channel)
-	rpio.Close()
+	defer p.Close()
+
+	// try to create a connection with parameters
+	_, err = p.Connect(physic.MegaHertz, spi.Mode0, 8)
+	if err != nil {
+		return nil, err
+	}
+
+	// err := rpio.Open()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// var channel rpio.SpiDev
+	// if adc.channel == 0 {
+	// 	channel = rpio.Spi0
+	// } else {
+	// 	channel = rpio.Spi1
+	// }
+	// err = rpio.SpiBegin(channel)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// rpio.SpiEnd(channel)
+	// rpio.Close()
 	return &adc, nil
 }
 
@@ -41,23 +69,45 @@ func (m *mcp3002) Read(channel uint8) (uint32, error) {
 	if channel > 1 {
 		return 0, errors.New("Wrong MCP3002 channel")
 	}
-	err := rpio.Open()
+
+	var spiChan = ""
+	if m.channel == 0 {
+		spiChan = "0"
+	} else {
+		spiChan = "1"
+	}
+
+	// open port
+	p, err := spireg.Open(spiChan)
 	if err != nil {
 		return 0, err
 	}
-	err = rpio.SpiBegin(rpio.SpiDev(m.channel))
+	defer p.Close()
+
+	// try to create a connection with parameters
+	c, err := p.Connect(physic.MegaHertz, spi.Mode0, 8)
 	if err != nil {
 		return 0, err
 	}
-	// CS
-	rpio.SpiChipSelect(m.csel)
+	// err := rpio.Open()
+	// if err != nil {
+	// 	return 0, err
+	// }
+	// err = rpio.SpiBegin(rpio.SpiDev(m.channel))
+	// if err != nil {
+	// 	return 0, err
+	// }
+	// // CS
+	// rpio.SpiChipSelect(m.csel)
 
 	// Start bit, single channel read
 	var command byte = 0b11010000
 	command |= channel << 5
 
 	txBuf := []byte{command, 0x00, 0x00}
-	rpio.SpiExchange(txBuf)
+	rxBuf := make([]byte, len(txBuf))
+	c.Tx(txBuf, rxBuf)
+	// rpio.SpiExchange(txBuf)
 
 	// convert value
 	var result uint32
@@ -65,7 +115,7 @@ func (m *mcp3002) Read(channel uint8) (uint32, error) {
 	result |= (uint32(txBuf[1]) & 0xff) << 1
 	result |= (uint32(txBuf[2]) & 0x80) >> 7
 
-	rpio.SpiEnd(rpio.SpiDev(m.channel))
-	rpio.Close()
+	//rpio.SpiEnd(rpio.SpiDev(m.channel))
+	//rpio.Close()
 	return result, nil
 }
