7dbc4b7c3c220ee7b32e36fe0fbad5c89d3a21df
David Pello <zako@ladecadence.net>
2025-04-27 12:30:37 +0200

    Fix paths

--- pkg/picture/picture_test.go
+++ pkg/picture/picture_test.go
@@ -16,15 +16,15 @@ func TestPictureData(t *testing.T) {
 }
 
 func TestPictureTake(t *testing.T) {
-	pic := New(0, "test", "/home/pi/eki2/images")
+	pic := New(0, "test", "/home/pi/eki2/image/")
 	pic.UpdateName()
 	pic.Capture()
+	fmt.Printf("Picture shot: %s\n", pic.Filename)
 
 	// now small picture
-	pic2 := New(0, "small", "/home/pi/eki2/images")
-	pic2.CaptureSmall("ssdv"+fmt.Sprintf("%d", pic2.Number), "640x480")
+	pic.CaptureSmall("ssdv", "640x480")
 
-	err := pic.AddInfo(pic2.Filename, "ID", "USBID", "Test image, message", "Data: 1234567890")
+	err := pic.AddInfo(pic.Path+"ssdv", "ID", "USBID", "Test image, message", "Data: 1234567890")
 	if err != nil {
 		t.Errorf("Problem adding info tom image: %v", err)
 	}
