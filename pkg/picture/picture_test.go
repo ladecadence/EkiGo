ea80113fc8b24cdc2496288ff4f266c95e12ac49
David Pello <zako@ladecadence.net>
2025-04-27 12:21:10 +0200

    Real rpicam test

--- pkg/picture/picture_test.go
+++ pkg/picture/picture_test.go
@@ -1,14 +1,32 @@
 package picture
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestPicture(t *testing.T) {
+func TestPictureData(t *testing.T) {
 	pic := New(0, "test", "./")
 
 	err := pic.AddInfo("test.jpg", "ID", "USBID", "Test image, message", "Data: 1234567890")
 	if err != nil {
 		t.Errorf("Problem adding info tom image: %v", err)
 	}
+
+}
+
+func TestPictureTake(t *testing.T) {
+	pic := New(0, "test", "/home/pi/eki2/images")
+	pic.UpdateName()
+	pic.Capture()
+
+	// now small picture
+	pic2 := New(0, "small", "/home/pi/eki2/images")
+	pic2.CaptureSmall("ssdv"+fmt.Sprintf("%d", pic2.Number), "640x480")
+
+	err := pic.AddInfo(pic2.Filename, "ID", "USBID", "Test image, message", "Data: 1234567890")
+	if err != nil {
+		t.Errorf("Problem adding info tom image: %v", err)
+	}
+
 }
