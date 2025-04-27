44a8f151c413ead71c6c25941b31e7838ce4f572
David Pello <zako@ladecadence.net>
2025-04-27 20:13:49 +0200

    First mission and subsystem tests

--- cmd/ekigo.go
+++ cmd/ekigo.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 
 	"github.com/ladecadence/EkiGo/pkg/config"
+	"github.com/ladecadence/EkiGo/pkg/logging"
+	"github.com/ladecadence/EkiGo/pkg/mission"
 )
 
 func main() {
@@ -12,6 +14,29 @@ func main() {
 	configFile := flag.String("c", "config.toml", "Config file")
 	flag.Parse()
 
-	conf := config.GetConfig(*configFile)
-	log.Printf("Mission %s starting...\n", conf.ID)
+	conf, err := config.GetConfig(*configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// now test that configuration is not the default one
+	if conf.ID() == "" || conf.SubID() == "" || conf.Msg() == "" ||
+		conf.Separator() == "" || conf.PathMainDir() == "" ||
+		conf.GpsPort() == "" {
+		fmt.Println("Please edit the configuration file.")
+		panic("")
+	}
+
+	// create mission and configure it
+	mission, err := mission.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Ok, now get time from GPS and update system time
+	err = mission.Gps().Update()
+	if err != nil {
+		mission.Log().Log(logging.LogError, fmt.Sprintf("Error updating GPS: %v", err))
+	}
+
 }
