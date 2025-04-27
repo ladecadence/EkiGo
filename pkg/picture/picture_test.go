6add69e72d8636b3d4ac4c3cfd7e306fc987d9a9
David Pello <zako@ladecadence.net>
2025-04-27 12:38:01 +0200

    Test for errors

--- pkg/picture/picture_test.go
+++ pkg/picture/picture_test.go
@@ -10,7 +10,7 @@ func TestPictureData(t *testing.T) {
 
 	err := pic.AddInfo("test.jpg", "ID", "USBID", "Test image, message", "Data: 1234567890")
 	if err != nil {
-		t.Errorf("Problem adding info tom image: %v", err)
+		t.Errorf("Problem adding info to image: %v", err)
 	}
 
 }
@@ -18,15 +18,22 @@ func TestPictureData(t *testing.T) {
 func TestPictureTake(t *testing.T) {
 	pic := New(0, "test", "/home/pi/eki2/image/")
 	pic.UpdateName()
-	pic.Capture()
+	err := pic.Capture()
+	if err != nil {
+		t.Errorf("Problem capturing image: %v", err)
+	}
 	fmt.Printf("Picture shot: %s\n", pic.Filename)
 
 	// now small picture
-	pic.CaptureSmall("ssdv", "640x480")
-
-	err := pic.AddInfo(pic.Path+"ssdv", "ID", "USBID", "Test image, message", "Data: 1234567890")
+	err = pic.CaptureSmall("ssdv.jpg", "640x480")
 	if err != nil {
-		t.Errorf("Problem adding info tom image: %v", err)
+		t.Errorf("Problem capturing small image: %v", err)
+	}
+	fmt.Printf("Picture shot: %s\n", pic.Filename)
+
+	err = pic.AddInfo(pic.Path+"ssdv.jpg", "ID", "USBID", "Test image, message", "Data: 1234567890")
+	if err != nil {
+		t.Errorf("Problem adding info to image: %v", err)
 	}
 
 }
