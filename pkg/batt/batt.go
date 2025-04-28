7bc666dde224b0d7cc68e724d1eab98ce24821a3
David Pello <zako@ladecadence.net>
2025-04-28 09:56:18 +0200

    Init with Mul and Div and added ReadRaw()

--- pkg/batt/batt.go
+++ pkg/batt/batt.go
@@ -13,16 +13,19 @@ import (
 )
 
 type Batt interface {
-	Read(mcp3002.MCP3002, uint8) (uint32, error)
+	ReadRaw(mcp3002.MCP3002, uint8) (uint32, error)
+	Read(mcp3002.MCP3002, uint8) (float64, error)
 }
 
 type batt struct {
 	EnPin gpio.PinIO
 	Ready bool
+	Mul   float64
+	Div   float64
 }
 
-func New(pin uint8) (Batt, error) {
-	b := batt{Ready: false}
+func New(pin uint8, mul, div float64) (Batt, error) {
+	b := batt{Ready: false, Mul: mul, Div: div}
 
 	// get state TODO
 	_, err := host.Init()
@@ -37,7 +40,7 @@ func New(pin uint8) (Batt, error) {
 	return &b, nil
 }
 
-func (b *batt) Read(adc mcp3002.MCP3002, channel uint8) (uint32, error) {
+func (b *batt) ReadRaw(adc mcp3002.MCP3002, channel uint8) (uint32, error) {
 	if !b.Ready {
 		return 0, errors.New("Not configured")
 	}
@@ -62,4 +65,33 @@ func (b *batt) Read(adc mcp3002.MCP3002, channel uint8) (uint32, error) {
 	}
 
 	return value, nil
+}
+
+func (b *batt) Read(adc mcp3002.MCP3002, channel uint8) (float64, error) {
+	if !b.Ready {
+		return 0, errors.New("Not configured")
+	}
+
+	// enable battery read
+	err := b.EnPin.Out(gpio.High)
+	if err != nil {
+		return 0, err
+	}
+	// wait 1ms for current to stabilize
+	time.Sleep(time.Millisecond * 1)
+
+	value, err := adc.Read(channel)
+	if err != nil {
+		return 0, err
+	}
+
+	// disable battery read
+	err = b.EnPin.Out(gpio.Low)
+	if err != nil {
+		return 0, err
+	}
+
+	vBatt := b.Mul * b.Div * (float64(value) * 3.3 / 1023.0)
+
+	return vBatt, nil
 }
